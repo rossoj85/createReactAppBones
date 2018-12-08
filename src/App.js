da3754f7916b4099641caaf78da2cb0d16924ab7
rossoj85 <jason_ro@photoninfotech.net>
2018-12-08 01:08:20 -0600

    changes registered?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
+import { Route, Switch, Fade, BrowserRouter as Router} from 'react-router-dom';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {AccessPage} from './components'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>HELLO!!</h1>
+          <Router>
+            <Route exact path='/' render={AccessPage} />
+          </Router>
       </div>
     );
   }
